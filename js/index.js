d881036371082c59025245899f38d32b2e735c56
SuicidePandaX <leonel.pereyra@outlook.com>
2022-11-27 13:37:36 -0300

    errores

--- js/index.js
+++ js/index.js
@@ -6,41 +6,43 @@ const mayorEdad = (edad) => {
     } else { return false};
 }
 
+function calcular(flag){
+
+    let vino = 1000;
+    let cigarro = 350;
+    let cantCigarro = 0;
+    let cantVino = 0;
+    let producto = '';
+
+    while(flag == 'si'){
+        producto = prompt("Que desea comprar: 'vino' $1000 ó 'cigarro' $350?");
+        switch(producto){
+            case 'vino':
+                cantVino = parseInt(prompt("Cuantos vinos?"));
+                totalProductos += vino*cantVino;
+                break;
+            case 'cigarro':
+                cantCigarro = parseInt(prompt("Cuantos cigarros?"));
+                totalProductos += cigarro*cantCigarro;
+                break;
+            default:
+                alert("Usted no ingresó un producto válido"); 
+                continue;   
+        }
+        flag = prompt("Desea agregar mas productos ingrese 'si' ó 'esc' para salir");
+    }
+}
+
 let edad = parseInt(prompt("Porfavor ingrese su edad para corroborar que tiene (18+) años"));
 
 if (mayorEdad(edad) === true){
     alert(`Usted es mayor de edad: ${edad}, disfrute nuestra app`);
-
-    //VARIABLES
-    let vino = 1000;
-    let cigarro = 350;
     let totalProductos = 0;
-    let producto = '';
-    let cantVino = 0;
-    let cantCigarro = 0;
-
     let flag = prompt("Bienvenido, si desea comenzar ingrese 'si', sino ingrese 'esc'");
-
     if(flag == 'esc'){
         alert("Usted finalizó el programa");
     } else if (flag == 'si'){
-        while(flag == 'si'){
-            producto = prompt("Que desea comprar: 'vino' ó 'cigarro'?");
-            switch(producto){
-                case 'vino':
-                    cantVino = parseInt(prompt("Cuantos vinos?"));
-                    totalProductos += vino*cantVino;
-                    break;
-                case 'cigarro':
-                    cantCigarro = parseInt(prompt("Cuantos cigarros?"));
-                    totalProductos += cigarro*cantCigarro;
-                    break;
-                default:
-                    alert("Usted no ingresó un producto válido"); 
-                    continue;
-            }
-            flag = prompt("Desea agregar mas productos ingrese 'si' ó 'esc' para salir");
-        }
+        calcular(flag);
         alert(`Usted va a comprar: ${cantVino} Vinos y ${cantCigarro} Cigarros por un total de: $${totalProductos} pesos`);
     }  else { 
         //Elimino la opcion de que ingrese una palabra no especificada
